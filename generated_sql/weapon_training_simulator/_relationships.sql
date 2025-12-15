28bf894746aaa792d163dcc974805de26825324b
prassaad <prasaddamgeti@gmail.com>
2025-12-15 13:10:39 +0530

    Generated FK constraints

--- generated_sql/weapon_training_simulator/_relationships.sql
+++ generated_sql/weapon_training_simulator/_relationships.sql
@@ -2,7 +2,7 @@
 -- FOREIGN KEY CONSTRAINTS
 -- ================================================
 ALTER TABLE public."training_sessions"
- ADD CONSTRAINT "FK_trainingSessions_trainees_traineeId"
- FOREIGN KEY ("trainee_id") REFERENCES public."trainees" ("id");
- ON DELETE SET NULL ON UPDATE CASCADE;
+    ADD CONSTRAINT "FK_training_sessions_trainees_trainee_id"
+    FOREIGN KEY ("trainee_id") REFERENCES public."trainees" ("id")
+    ON DELETE SET NULL ON UPDATE CASCADE;
 
